4f5244fbb00f9589c326b305b8f9fd94cac94709
pxai <pello_altadill@cuatrovientos.org>
2018-03-05 17:53:39 +0100

    ok, done

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,19 +2,28 @@ console.log('Here we are: ', process.argv)
 const file = require('fs')
 const _ = require('lodash')
 const notes = require('./notes')
-var command = process.argv[2];
+const yargs = require('yargs');
+
+const argv = yargs.argv;
+var command = argv._[0];
+
+console.log(argv)
+console.log(process.argv)
 console.log(`Received command: ${command}`)
 if (command === 'add') {
-	console.log('Adding note')
+	console.log('Adding note : ', argv.title, argv.body)
+	notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
 	console.log('List command');
+	notes.getAll();
 } else if ( command === 'show') {
 	console.log('Show command');
+	notes.getNote(argv.title)
 } else if ( command === 'update'){
 	console.log('Update command');
 } else if ( command === 'delete') {
 	console.log('Delete command');
+	notes.removeNote(argv.title);
 } else {
 	console.log('Unknown command');
 }
-notes.addNote()
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,7 +1,24 @@
 
 // What happens with arrows?
 // does not bidn this or args array!!!!
-module.exports.addNote = () => {
-	console.log('ady2yy2yd note')
+var addNote = (title, body) => {
+	console.log('added note', title, body)
 	return 'Added note'
+}
+var getAll = () => {
+	console.log('Showing all');
+}
+
+var getNote = (title) => {
+	console.log('Getting note', title)
+}
+
+var removeNote = (title) => {
+	console.log('Note removed, title');
+}
+module.exports = {
+	addNote,
+	getAll,
+	getNote,
+	removeNote
 }
