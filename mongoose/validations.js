6f319f7950a1e6a32518db4e30e778ac92658f76
pxai <pello_altadill@cuatrovientos.org>
2018-03-27 09:08:09 +0200

    ok, dan

--- mongoose/validations.js
+++ mongoose/validations.js
@@ -5,15 +5,28 @@ mongoose.connect('mongodb://localhost:27017/sample');
 
 const Friend = mongoose.model('Friend', {
   name: {
-    type: String
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3
   },
   email: {
-    type: String
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 5,
+    default: null
+  },
+  age: {
+    type: Number,
+    required: false,
+    default: 42
   }
 });
 
 const newFriend = new Friend({
-  name: 'Imaginary'
+  name: 'Imaginary',
+  email: 'none@wer.com'
 });
 
 newFriend.save().then((friend) => {
