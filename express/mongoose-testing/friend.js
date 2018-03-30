5bf86e46ef529d98f181ba0cc629948202e1d7a3
pxai <pello_altadill@cuatrovientos.org>
2018-03-30 17:19:01 +0200

    ok, done for today

---
+++ express/mongoose-testing/friend.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise; // Use default promises
+mongoose.connect('mongodb://localhost:27017/sample');
+
+const Friend = mongoose.model('Friend', {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 5,
+    default: null
+  },
+  age: {
+    type: Number,
+    required: false,
+    default: 42
+  }
+});
+
+module.exports = Friend
+/*const newFriend = new Friend({
+  name: 'Imaginary',
+  email: 'none@wer.com'
+});
+
+newFriend.save().then((friend) => {
+  console.log('Saved friend: ', friend);
+}, (e) => {
+  console.log('Error', e);
+})*/
---
+++ express/mongoose-testing/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const hbs = require('hbs');
+const Friend = require('./friend');
+
+var app = express();
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+app.use(express.static(__dirname + '/public'));
+
+//hbs.registerHelper('each');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/ok', (req, res) => {
+    res.status(200).send('All right all right all right');
+});
+
+app.get('/friends', (req, res) => {
+    Friend.find().then((friends) => {
+      console.log(friends)
+      res.status(200).render('friends', {friends});
+    }).catch((err) => {
+      if (err) return res.status(404).send(err);
+    })
+});
+
+app.get('/api/friends', (req, res) => {
+    Friend.find().then((friends) => {
+      res.status(200).send(friends);
+    }).catch((err) => {
+      if (err) return res.status(404).send(err);
+    })
+
+});
+
+app.get('/api/friends/:id', (req, res) => {
+  console.log(req.params.id);
+    Friend.find({_id: req.params.id}).then((friend) => {
+      res.status(200).send(friend);
+    }).catch((err) => {
+      if (err) return res.status(404).send(err);
+    })
+
+});
+
+
+app.listen(3000, () => {
+    console.log('This works on 3000, you don\'t');
+});
+
+module.exports.app = app;
