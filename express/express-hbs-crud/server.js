7e26bb91a082451cd0ed4085330d5d4e1ac4b065
pxai <pello_altadill@cuatrovientos.org>
2018-04-07 17:49:25 +0200

    Ok, added all crud cases

--- express/express-hbs-crud/server.js
+++ express/express-hbs-crud/server.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const hbs = require('hbs');
 const Task = require('./task');
+const bodyParser = require('body-parser')
 
+hbs.registerHelper('select', function(selected, options) {
+  console.log('Select', selected, options);
+    return options.fn(this).replace(
+        new RegExp(' value=\"' + selected + '\"'),
+        '$& selected="selected"');
+});
 var app = express();
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(__dirname + '/public'));
-
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 //hbs.registerHelper('each');
+
 
 app.get('/', (req, res) => {
     res.render('index');
 });
+
 
 
 app.get('/tasks', (req, res) => {
@@ -23,9 +34,10 @@ app.get('/tasks', (req, res) => {
     })
 });
 
-app.get('/tasks/:id', (req, res) => {
+
+app.get('/tasks/detail/:id', (req, res) => {
   console.log(req.params.id);
-    Friend.find({_id: req.params.id}).then((task) => {
+    Task.findById({_id: req.params.id}).then((task) => {
         res.status(200).render('detail', {task});
     }).catch((err) => {
       if (err) return res.status(404).send(err);
@@ -34,15 +46,62 @@ app.get('/tasks/:id', (req, res) => {
 });
 
 
-app.get('/friends/delete/:id', (req, res) => {
+app.get('/tasks/delete/:id', (req, res) => {
   console.log(req.params.id);
-    Friend.remove({_id: req.params.id}).then((friend) => {
-      res.status(200).redirect('/friends');
+    Task.remove({_id: req.params.id}).then((friend) => {
+      res.status(200).redirect('/tasks');
     }).catch((err) => {
       if (err) return res.status(404).render('404');
     })
 });
 
+app.get('/tasks/new', (req, res) => {
+      res.status(200).render('new');
+});
+
+app.post('/tasks/new', (req, res) => {
+  console.log('sent post: ', req.body.name, req.body.done);
+  const newTask = new Task({
+    name: req.body.name,
+    date: new Date(),
+    done: req.body.done
+  });
+
+  newTask.save().then((task) => {
+    console.log('Created: ', task)
+    res.status(200).redirect('/tasks');
+  }).catch((err) => {
+    console.log('Error¿?: ', err)
+    if (err) return res.status(404).send(err);
+  })
+});
+
+app.get('/tasks/update/:id', (req, res) => {
+  Task.findById({_id: req.params.id}).then((task) => {
+      res.status(200).render('update', {task});
+  }).catch((err) => {
+    if (err) return res.status(404).send(err);
+  })
+});
+
+app.post('/tasks/update', (req, res) => {
+  console.log('sent post: ', req.body._id, req.body.name, req.body.done);
+  const newTask = {
+    name: req.body.name,
+    date: new Date(),
+    done: req.body.done
+  };
+
+  Task.findByIdAndUpdate({_id: req.body._id},
+    { $set: newTask },
+    { new: true}).then((task) => {
+    console.log('Updated: ', task)
+    res.status(200).redirect('/tasks');
+  }).catch((err) => {
+    console.log('Error¿?: ', err)
+    if (err) return res.status(404).send(err);
+  })
+});
 
 app.listen(3000, () => {
     console.log('This works on 3000, you don\'t');
--- express/express-hbs-crud/task.js
+++ express/express-hbs-crud/task.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/task');
+mongoose.connect('mongodb://localhost:27017/tasks');
 
 const Task = mongoose.model('Task', {
   name: {
