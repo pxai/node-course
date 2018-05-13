35f1465527be918530904cb572e938911a0f57ff
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-05-13 19:08:50 +0200

    Ok, async await, tested as promises

--- async-samples/asyncawait.math.test.js
+++ async-samples/asyncawait.math.test.js
@@ -2,9 +2,18 @@ const expect = require('expect');
 const { add, sub, mul, div } = require('./asyncawait.math.js');
 
 describe('Testing for async await math functions', () => {
-    it('Should add correctly',() => {
-		const result = await add(40,2);
-		expect(result).toEqual(42);
+    it('Should add correctly',(done) => {
+		add(40,2).then(result => {
+			expect(result).toEqual(42);
+			done();
+		});
     });
+
+	it('should sub correctly', (done) => {
+		sub(44,2).then(result => {
+			expect(result).toEqual(42);
+			done();
+		});
+	});
 }); 
 
